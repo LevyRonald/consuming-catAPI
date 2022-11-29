2e0d1277c8de3fa8cb760c23c1f89087fd5b44e7
Levy <levyronald3@gmail.com>
2022-11-29 14:08:42 -0300

    feat: adicionando sidebar

--- src/components/Navbar/NavbarApp.vue
+++ src/components/Navbar/NavbarApp.vue
@@ -1,13 +1,26 @@
 <template>
+  <sidebar />
   <div>
-    <b-icon icon="gear-fill"></b-icon>
     <b-navbar type="dark" variant="dark">
       <b-navbar-nav class="d-flex justify-items-center">
         <b-nav-item class="nav-items">
-            <b-button variant="outline-primary">Menu</b-button>
+          <b-button
+            variant="outline-primary"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasExample"
+            >Menu</b-button
+          >
         </b-nav-item>
         <b-navbar-brand>Cats Curiosity</b-navbar-brand>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Sidebar from "../Sidebar/SidebarApp.vue";
+
+export default defineComponent({
+  components: { Sidebar },
+});
+</script>
---
+++ src/components/Sidebar/SidebarApp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="offcanvas offcanvas-start"
+    tabindex="-1"
+    id="offcanvasExample"
+    aria-labelledby="offcanvasExampleLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+      <button
+        type="button"
+        class="btn-close text-reset"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <div>
+        <p>Home</p>
+        <p>Favoritos</p>
+      </div>
+      <div class="dropdown mt-3"></div>
+    </div>
+  </div>
+</template>
