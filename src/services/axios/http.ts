571565d1ad1891f01a53806a912522840353c864
Levy <levyronald3@gmail.com>
2022-11-29 10:14:44 -0300

    feat: iniciando configuração do vuex

---
+++ src/services/axios/http.ts
@@ -0,0 +1,10 @@
+const API_URL = 'https://api.thecatapi.com/v1/';
+import axios from 'axios';
+
+export const http = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'accept': 'application/json',
+        'content': 'application/json'
+    },
+});
