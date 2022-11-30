6547a480a96798c5505c110f4814f92b5fd71a6b
Levy <levyronald3@gmail.com>
2022-11-30 15:08:23 -0300

    feat: componente de select e logica para renderizar opções

---
+++ src/components/Select/SelectApp.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-form-select :options="NameBreeds"></b-form-select>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        NameBreeds: {
+            type: Array
+        }
+    }
+})
+</script>
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
+  <select-app :NameBreeds="selectBreeds" />
   <cats-card :breeds="breeds" />
   <div class="w-100 d-flex justify-content-end">
-    <pagination v-model="page" :page="limit" :rows="rows" @click="handlePageChange" />
+    <pagination
+      v-model="page"
+      :page="limit"
+      :rows="rows"
+      @click="handlePageChange"
+    />
   </div>
 </template>
 <script lang="ts">
@@ -9,11 +15,18 @@ import { defineComponent } from "vue";
 import CatsCard from "@/components/Cards/CatsCard.vue";
 import Pagination from "@/components/Pagination/PaginationApp.vue";
 import CatsRequests from "@/services/axios/CatsRequests";
+import SelectApp from "@/components/Select/SelectApp.vue"
 
 export default defineComponent({
-  components: { CatsCard, Pagination },
+  components: { CatsCard, Pagination, SelectApp },
   data() {
     return {
+      selectBreeds: [''],
+      nameBreeds: [
+        {
+            name: ''
+        }
+      ],
       limit: 9,
       page: 0,
       rows: 1,
@@ -31,18 +44,20 @@ export default defineComponent({
         page: this.page,
       }).then((result) => {
         this.breeds = result.data;
-        console.log(this.breeds);
       });
     },
     handlePageChange() {
       this.page;
       this.renderCats();
     },
-    getRows() {
-        CatsRequests.getlAllBreeds().then((result) => {
+    async getRows() {
+        await CatsRequests.getlAllBreeds().then((result) => {
             this.rows = result.data.length
-            console.log(result);
+            this.nameBreeds = result.data
         })
+        for(let index = 0; index < this.rows; index++) {
+            this.selectBreeds.push(this.nameBreeds?.[index].name)
+        }
     }
   },
 });
