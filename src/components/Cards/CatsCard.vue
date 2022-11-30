c23b5c70b7ccf5a3e10f54783d9a2a59f5cc6be9
Levy <levyronald3@gmail.com>
2022-11-30 11:21:00 -0300

    feat: tratativa para cards sem fotos

--- src/components/Cards/CatsCard.vue
+++ src/components/Cards/CatsCard.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row cols="3">
     <b-col v-for="breed in breeds" :key="breed" class="pb-4">
-      <b-card :img-src="breed['image'].url" img-top>
+      <b-card :img-src="breed['image']?.url ? breed['image'].url : '/img/unnamed.jpg'" img-top>
         <div></div>
         <b-badge variant="info">{{ breed['origin'] }}</b-badge>
         <h4>{{ breed['name'] }}</h4>
