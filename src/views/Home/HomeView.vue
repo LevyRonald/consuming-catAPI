0866f10a9880ba22abde95fb0e191cea9583cc93
Levy <levyronald3@gmail.com>
2022-11-30 11:02:44 -0300

    feat: paginação funcionando

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <cats-card :breeds="breeds" />
-  <pagination />
+  <pagination v-model="page" :page="limit" :rows="rows" @click="handlePageChange" />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import CatsCard from "@/components/Cards/CatsCard.vue";
-import Pagination from '@/components/Pagination/PaginationApp.vue'
+import Pagination from "@/components/Pagination/PaginationApp.vue";
 import CatsRequests from "@/services/axios/CatsRequests";
 
 export default defineComponent({
@@ -13,15 +13,16 @@ export default defineComponent({
   data() {
     return {
       limit: 9,
-      page: 0,
+      page: 1,
+      rows: 67,
       breeds: {},
     };
   },
   mounted() {
-    this.renderCats();
+    this.handlePageChange()
   },
   methods: {
-    renderCats() {
+     renderCats() {
       CatsRequests.getlAllBreeds({
         limit: this.limit,
         page: this.page,
@@ -29,6 +30,10 @@ export default defineComponent({
         this.breeds = result.data;
         console.log(this.breeds);
       });
+    },
+    handlePageChange() {
+      this.page + 1;
+      this.renderCats();
     },
   },
 });
