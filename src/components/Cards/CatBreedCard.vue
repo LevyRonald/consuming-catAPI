62f5084f58276c1a8cb1746f7ee9c8607ec95e34
Levy <levyronald3@gmail.com>
2022-12-01 15:24:38 -0300

    feat: adicionando componente de carousel

--- src/components/Cards/CatBreedCard.vue
+++ src/components/Cards/CatBreedCard.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        <b-card>{{currentBreed}}</b-card>
-    </div>
+  <div>
+    <b-card>
+        <div>
+            <carousel-app />
+            {{currentBreed}}
+        </div>
+    </b-card>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import CarouselApp from "../Carousel/CarouselApp.vue";
 
 export default defineComponent({
+  components: { CarouselApp },
   props: {
     currentBreed: {
       type: Object,
---
+++ src/components/Carousel/CarouselApp.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-carousel
+    :interval="5000"
+    controls
+    indicators
+    background="#ababab"
+    img-width="1024"
+    img-height="280"
+    style="text-shadow: 1px 1px 2px #333"
+    class="carousel-dark carousel-fade"
+    >
+      <b-carousel-slide active img-src="https://picsum.photos/1024/480/?image=20" />
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=21" />
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=22" />
+  </b-carousel>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        imageCat: {
+            type: String
+        }
+    }
+})
+</script>
