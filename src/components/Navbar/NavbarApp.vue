1261772aa33937dee57dd562c0e71e93468fcf08
Levy <levyronald3@gmail.com>
2022-12-19 16:07:42 -0300

    refactor: refactoring navbar component

--- src/components/Navbar/NavbarApp.vue
+++ src/components/Navbar/NavbarApp.vue
@@ -1,20 +1,20 @@
 <template>
   <sidebar />
-  <div>
-    <b-navbar type="dark" variant="dark">
-      <b-navbar-nav class="d-flex justify-items-center">
-        <b-nav-item class="nav-items">
-          <b-button
-            variant="outline-primary"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasExample"
-            >Menu</b-button
-          >
-        </b-nav-item>
-        <b-navbar-brand>Cats Curiosity</b-navbar-brand>
-      </b-navbar-nav>
-    </b-navbar>
-  </div>
+  <b-navbar type="dark" variant="dark">
+    <div>
+      <b-button
+        variant="outline-primary"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasExample"
+        >Menu</b-button
+      >
+    </div>
+    <div class="w-100">
+      <div class="d-flex justify-content-center">
+        <h5 class="title" @click="redirect">Cats Curiosity</h5>
+      </div>
+    </div>
+  </b-navbar>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -22,5 +22,10 @@ import Sidebar from "../Sidebar/SidebarApp.vue";
 
 export default defineComponent({
   components: { Sidebar },
+  methods: {
+    redirect() {
+      this.$router.push({ name: "home" });
+    },
+  },
 });
 </script>
