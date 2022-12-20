877226e5e06b3574bead3a0ad4d3eb95e8ba134c
Levy <levyronald3@gmail.com>
2022-12-20 16:47:07 -0300

    feat: adicionando background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="pb-4">
     <navbar/>
+    <background class="backg"/>
   </nav>
   <div class="pe-5 ps-5">
     <router-view/>
@@ -9,14 +10,22 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Navbar from '@/components/Navbar/NavbarApp.vue'
+import background from '@/components/background/BackgroundApp.vue'
 
 export default defineComponent({
-  components: { Navbar }
+  components: { Navbar, background }
   
 })
 </script>
 
 <style>
+.backg{
+  position: absolute;
+  z-index: -1;
+  /* width: 100%;
+  height: 100vh;
+  overflow: hidden; */
+}
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
