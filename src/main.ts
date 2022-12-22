fab2b91de60cc61094f9601adeda5442da610b3f
Levy <levyronald3@gmail.com>
2022-12-22 17:51:41 -0300

    feat: adding new component of icons

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import router from './router'
 import store from './store'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import vue3StarRatings from "vue3-star-ratings";
+import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import 'vue3-carousel/dist/carousel.css'
 import '@/scss/main.scss';
 
-createApp(App).use(store).use(router).use(BootstrapVue3).use(vue3StarRatings).mount('#app')
+createApp(App).use(store).use(router).use(BootstrapVue3).use(vue3StarRatings).use(BootstrapIcon)
+    .mount('#app')
