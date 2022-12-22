30b68374e4c1649405a72136430fef04203d8c50
Levy <levyronald3@gmail.com>
2022-12-22 08:23:29 -0300

    feat: adding tratative to ratings lower that zero

--- src/components/Cards/CatBreedCard.vue
+++ src/components/Cards/CatBreedCard.vue
@@ -40,7 +40,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.affection_level > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Affection:</span>
                   <RatingStarVue
@@ -49,7 +49,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.child_friendly > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">child friendly:</span>
                   <RatingStarVue
@@ -58,7 +58,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.dog_friendly > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Dog friendly:</span>
                   <RatingStarVue
@@ -67,7 +67,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.energy_level > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Energy level:</span>
                   <RatingStarVue
@@ -76,7 +76,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.grooming > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Grooming:</span>
                   <RatingStarVue
@@ -85,7 +85,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.health_issues > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Health issues:</span>
                   <RatingStarVue
@@ -94,7 +94,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.intelligence > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Intelligence:</span>
                   <RatingStarVue
@@ -103,7 +103,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.shedding_level > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Sheeding level:</span>
                   <RatingStarVue
@@ -112,7 +112,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.social_needs > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Social needs:</span>
                   <RatingStarVue
@@ -121,7 +121,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.stranger_friendly > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Stranger friendly:</span>
                   <RatingStarVue
@@ -130,7 +130,7 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.social_needs > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Vocalisation:</span>
                   <RatingStarVue
@@ -139,12 +139,66 @@
                   />
                 </div>
               </b-col>
-              <b-col cols="6">
+              <b-col cols="6" v-if="currentBreed.experimental > 0">
                 <div class="d-flex flex-column">
                   <span class="colorText">Experimental:</span>
                   <RatingStarVue
                     class="d-flex"
                     :rating-raw-value="currentBreed.experimental"
+                  />
+                </div>
+              </b-col>
+              <b-col cols="6" v-if="currentBreed.hairless > 0">
+                <div class="d-flex flex-column">
+                  <span class="colorText">Hairless:</span>
+                  <RatingStarVue
+                    class="d-flex"
+                    :rating-raw-value="currentBreed.hairless"
+                  />
+                </div>
+              </b-col>
+              <b-col cols="6" v-if="currentBreed.natural > 0">
+                <div class="d-flex flex-column">
+                  <span class="colorText">Natural:</span>
+                  <RatingStarVue
+                    class="d-flex"
+                    :rating-raw-value="currentBreed.natural"
+                  />
+                </div>
+              </b-col>
+              <b-col cols="6" v-if="currentBreed.rare > 0">
+                <div class="d-flex flex-column">
+                  <span class="colorText">Rare:</span>
+                  <RatingStarVue
+                    class="d-flex"
+                    :rating-raw-value="currentBreed.rare"
+                  />
+                </div>
+              </b-col>
+              <b-col cols="6" v-if="currentBreed.suppressed_tail > 0">
+                <div class="d-flex flex-column">
+                  <span class="colorText">Suppressed tail:</span>
+                  <RatingStarVue
+                    class="d-flex"
+                    :rating-raw-value="currentBreed.suppressed_tail"
+                  />
+                </div>
+              </b-col>
+              <b-col cols="6" v-if="currentBreed.short_legs > 0">
+                <div class="d-flex flex-column">
+                  <span class="colorText">Short legs:</span>
+                  <RatingStarVue
+                    class="d-flex"
+                    :rating-raw-value="currentBreed.short_legs"
+                  />
+                </div>
+              </b-col>
+              <b-col cols="6" v-if="currentBreed.hypoallergenic > 0">
+                <div class="d-flex flex-column">
+                  <span class="colorText">Hypoallergenic:</span>
+                  <RatingStarVue
+                    class="d-flex"
+                    :rating-raw-value="currentBreed.hypoallergenic"
                   />
                 </div>
               </b-col>
