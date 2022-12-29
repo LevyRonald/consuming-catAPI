f960123738eb541833bbf7c2410cf9dc9166207b
Levy <levyronald3@gmail.com>
2022-12-29 09:45:26 -0300

    feat: adding redirect in cards home to breed view

--- src/components/Cards/CatsCard.vue
+++ src/components/Cards/CatsCard.vue
@@ -1,11 +1,17 @@
 <template>
   <b-row cols="3">
     <b-col v-for="breed in breeds" :key="breed" class="pb-4">
-      <b-card class="align-items-center pt-2" :img-src="breed['image']?.url ? breed['image'].url : '/img/unnamed.jpg'" img-top>
-        <div></div>
-        <b-badge variant="info">{{ breed['origin'] }}</b-badge>
-        <h4 class="colorText">{{ breed['name'] }}</h4>
-        <p class="colorText">{{ breed['description'].substring(0, 120) + '...'}}</p>
+      <b-card
+        class="cardClick align-items-center pt-2"
+        :img-src="breed['image']?.url ? breed['image'].url : '/img/unnamed.jpg'"
+        img-top
+        @click="redirectBreed(breed.id)"
+      >
+        <b-badge variant="info">{{ breed["origin"] }}</b-badge>
+        <h4 class="colorText">{{ breed["name"] }}</h4>
+        <p class="colorText">
+          {{ breed["description"].substring(0, 120) + "..." }}
+        </p>
       </b-card>
     </b-col>
   </b-row>
@@ -22,6 +28,11 @@ export default defineComponent({
   props: {
     breeds: {
       type: Object,
+    },
+  },
+  methods: {
+    redirectBreed(breedId: string) {
+      this.$router.push({path: `/${breedId}`})
     }
   }
 });
